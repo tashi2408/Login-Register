44e06ca2f49034f83a963f6a1ccd136214d8e72a
tashi2408 <63187583+tashi2408@users.noreply.github.com>
2021-05-09 12:39:09 +0530

    Updates

---
+++ precache-manifest.f125e51c030aa3536d4b24e69a517cde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba0e29b4d133b30546806a66782e2711",
+    "url": "/Login-Register/index.html"
+  },
+  {
+    "revision": "b1e0fbbda90fb4e3b142",
+    "url": "/Login-Register/static/js/2.adb8c2f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Login-Register/static/js/2.adb8c2f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5312bca7ff570bc6f0da",
+    "url": "/Login-Register/static/js/main.b28ee6b2.chunk.js"
+  },
+  {
+    "revision": "a4312cfd8693f37f93ed",
+    "url": "/Login-Register/static/js/runtime-main.9f09c65f.js"
+  }
+]);
